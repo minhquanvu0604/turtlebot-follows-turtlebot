8a45b38d75c70d61c576ec686127bdb9b869c828
anhminhtu <anhminh.tu@student.uts.edu.au>
2023-10-06 01:09:18 +1100

    fix conflict

--- turtlebot_follows_turtlebot_navigation/scripts/aruco_detection_depth.py
+++ turtlebot_follows_turtlebot_navigation/scripts/aruco_detection_depth.py
@@ -4,7 +4,6 @@ import rospy
 import cv2
 import cv2.aruco as aruco
 
-# from cv2 import aruco
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CameraInfo
@@ -15,10 +14,10 @@ class ArUcoDepthDetector:
         self.bridge = CvBridge()
 
         # Subscribers
-        self.rgb_subscriber = rospy.Subscriber("follower/camera/rgb/image_raw", Image, self.rgb_callback)
-        self.depth_subscriber = rospy.Subscriber("follower/camera/depth/image_raw", Image, self.depth_callback)
+        self.rgb_subscriber = rospy.Subscriber("camera/color/image_raw", Image, self.rgb_callback)
+        self.depth_subscriber = rospy.Subscriber("camera/depth/image_raw", Image, self.depth_callback)
 
-        self.camera_info = rospy.wait_for_message("follower/camera/rgb/camera_info", CameraInfo)
+        self.camera_info = rospy.wait_for_message("camera/color/camera_info", CameraInfo)
         self._dist_coeffs = np.zeros((4,1))
         self._camera_matrix = np.array(self.camera_info.K).reshape(3, 3)
         # Publisher
@@ -38,18 +37,17 @@ class ArUcoDepthDetector:
             return
 
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        # aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
-        # parameters = aruco.DetectorParameters()
+        aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+        parameters = aruco.DetectorParameters()
 
-        aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-        parameters = aruco.DetectorParameters_create()
+        # aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        # parameters = aruco.DetectorParameters_create()
 
         corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
         if ids is not None:
             print("Found ArUco marker!")
             rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners[0], 0.05, self._camera_matrix , self._dist_coeffs)
-
             # Using depth information to refine or validate the pose estimation
             marker_center = np.mean(corners[0][0], axis=0).astype(int)
             depth_at_marker = self.latest_depth_image[marker_center[1], marker_center[0]]
