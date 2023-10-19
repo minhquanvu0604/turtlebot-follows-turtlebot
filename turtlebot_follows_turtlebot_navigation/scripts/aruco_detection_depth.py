70648bf6ed083e0719455159a2efc2d3684629a4
Anh Minh Tu <anhminh.tu@student.uts.edu.au>
2023-10-19 16:47:58 +1100

    update aruco detection node

--- turtlebot_follows_turtlebot_navigation/scripts/aruco_detection_depth.py
+++ turtlebot_follows_turtlebot_navigation/scripts/aruco_detection_depth.py
@@ -4,6 +4,7 @@ import rospy
 import cv2
 import cv2.aruco as aruco
 
+
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CameraInfo
@@ -14,12 +15,13 @@ class ArUcoDepthDetector:
         self.bridge = CvBridge()
 
         # Subscribers
-        self.rgb_subscriber = rospy.Subscriber("camera/color/image_raw", Image, self.rgb_callback)
-        self.depth_subscriber = rospy.Subscriber("camera/depth/image_raw", Image, self.depth_callback)
+        self.rgb_subscriber = rospy.Subscriber("/follower/camera/rgb/image_raw", Image, self.rgb_callback)
+        # self.depth_subscriber = rospy.Subscriber("camera/depth/image_raw", Image, self.depth_callback)
 
-        self.camera_info = rospy.wait_for_message("camera/color/camera_info", CameraInfo)
+        self.camera_info = rospy.wait_for_message("/follower/camera/rgb/camera_info", CameraInfo)
         self._dist_coeffs = np.zeros((4,1))
         self._camera_matrix = np.array(self.camera_info.K).reshape(3, 3)
+        
         # Publisher
         self.pose_publisher = rospy.Publisher("/aruco_depth_pose", Pose, queue_size=10)
 
@@ -29,8 +31,6 @@ class ArUcoDepthDetector:
     def rgb_callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            cv2.imshow("RGB Image", cv_image)
-            cv2.waitKey(1)
 
         except CvBridgeError as e:
             print(e)
@@ -48,24 +48,29 @@ class ArUcoDepthDetector:
         if ids is not None:
             print("Found ArUco marker!")
             rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners[0], 0.05, self._camera_matrix , self._dist_coeffs)
+            aruco.drawDetectedMarkers(cv_image,corners,ids)
+
             # Using depth information to refine or validate the pose estimation
-            marker_center = np.mean(corners[0][0], axis=0).astype(int)
-            depth_at_marker = self.latest_depth_image[marker_center[1], marker_center[0]]
+            # marker_center = np.mean(corners[0][0], axis=0).astype(int)
+            # depth_at_marker = self.latest_depth_image[marker_center[1], marker_center[0]]
 
             # Use depth_at_marker to refine tvec or validate its accuracy.
 
             pose = Pose()
-            # Fill the pose data based on the refined or validated tvec and rvec
 
+            # Fill the pose data based on the refined or validated tvec and rvec
             self.pose_publisher.publish(pose)
         else: 
             print("No ArUco marker found!")
 
-    def depth_callback(self, data):
-        try:
-            self.latest_depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
-        except CvBridgeError as e:
-            print(e)
+        cv2.imshow("Detected ArUco markers", cv_image)
+        cv2.waitKey(1)
+
+    # def depth_callback(self, data):
+    #     try:
+    #         self.latest_depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
+    #     except CvBridgeError as e:
+    #         print(e)
 
 if __name__ == '__main__':
     rospy.init_node('aruco_depth_detector_node', anonymous=True)
